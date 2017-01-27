ce1b33372b412b446668d5ae6a7f8efe5d5270e7
Thalida Noel <thalida.c.noel@gmail.com>
2017-01-26 23:50:22 -0500

    Twitter: Updated component

--- app/components/twitter/twitter.component.js
+++ app/components/twitter/twitter.component.js
@@ -6,29 +6,32 @@ module.exports = {
         url: '@'
     },
     controller: [
+        '$element',
         '$sce',
         '$timeout',
         '$http',
-        function($sce, $timeout, $http){
+        function($element, $sce, $timeout, $http){
             var $ctrl = this;
 
             $ctrl.$onInit = function(){
+                $ctrl.$twitterEl = $element.find('.twitter-component');
                 $ctrl.render = false;
                 $ctrl.embed = null;
                 $ctrl.getEmbed();
             };
 
             $ctrl.getEmbed = function () {
-                var queryUrl = $sce.trustAsResourceUrl('https://publish.twitter.com/oembed?hide_thread=true&url=' + $ctrl.url);
+                var queryUrl = $sce.trustAsResourceUrl('https://publish.twitter.com/oembed?omit_script=true&url=' + $ctrl.url);
 
                 $http
                     .jsonp(queryUrl, {jsonpCallbackParam: 'callback'})
                     .then(function (res) {
+                        $ctrl.render = true;
                         $ctrl.embed = $sce.trustAsHtml(res.data.html);
 
-                        $timeout(function () {
-                            $ctrl.render = true;
-                        }, 300);
+                        $timeout(function(){
+                            twttr.widgets.load();
+                        }, 10)
                     });
             };
         }
