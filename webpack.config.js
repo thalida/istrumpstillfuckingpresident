bf014da9ff138d29e710a0e8f42554d20d5a1ec7
Thalida Noel <thalida.c.noel@gmail.com>
2017-01-28 18:34:49 -0500

    Dev test

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ var common = {
         path: DIST,
         filename: "[name].[hash].js",
         chunkFilename: "[id].js",
-        publicPath: ''
+        publicPath: '/istrumpstillfuckingpresident/'
     },
     module: {
         loaders: [
